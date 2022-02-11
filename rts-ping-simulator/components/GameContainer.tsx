7f1083bef59543430a0b6dec99d53c435b42cb97
Aearsears <53102185+Aearsears@users.noreply.github.com>
2022-02-10 23:05:01 -0500

    init nextjs project

---
+++ rts-ping-simulator/components/GameContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+class GameContainer extends React.Component {
+    static async getInitialProps() {
+        return {};
+    }
+    render(): React.ReactNode {
+        return <div>GameContainer</div>;
+    }
+}
+
+export default GameContainer;
